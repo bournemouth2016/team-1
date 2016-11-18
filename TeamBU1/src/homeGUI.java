7dbeff13e33f2b4801acb4e9c14e418596e8fe66
andre92764 <andre.redmer@protonmail.ch>
2016-11-18 20:26:07 +0000

    Add files via upload

---
+++ TeamBU1/src/homeGUI.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+
+
+public class homeGUI {
+
+	JFrame frame;
+	JPanel panel;
+	JButton btnDaily;
+	JLabel lblWelcome;
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
