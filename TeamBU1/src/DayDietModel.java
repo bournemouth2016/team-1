86d0a90fc6796473b298087f7dbd749bc869bbee
Richie <rmb481@cs.bham.ac.uk>
2016-11-18 21:13:30 +0000

    #OneDoesNotSimply implement an Observer Pattern with less than 6 classes observing each other

---
+++ TeamBU1/src/DayDietModel.java
@@ -0,0 +1,8 @@
+/**
+ * codeForGood
+ * Created by riddle on 11/18/16.
+ *
+ * @author rmb481@cs.bham.ac.uk
+ */
+public class DayDietModel {
+}
---
+++ TeamBU1/src/DayDietView.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * codeForGood
+ * Created by riddle on 11/18/16.
+ *
+ * @author rmb481@cs.bham.ac.uk
+ */
+public class DayDietComp extends JPanel {
+
+    public DayDietComp (DayDietModel model) {
+
+    }
+}
